89a7c3e2c42c4162d298f981672cb8651124bdf4
laban <laban2real@gmail.com>
2024-09-03 15:10:01 +0300

    age

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const app = // main.js
-Vue.createApp({
+const app = Vue.createApp({
   data() {
     return {
       user: {
@@ -8,8 +7,23 @@ Vue.createApp({
         location: 'New York',
         skills: ['JavaScript', 'HTML', 'CSS'],
         login: 'johndoe',
-        profilePicture: 'images/Ken-Carson.webp'
-      }
+        profilePicture: 'images/Ken-Carson.webp',
+        dob: '1990-01-01' // date of birth
+      },
+      showAgeFlag: false,
+      age: null
+    }
+  },
+  methods: {
+    showAge() {
+      this.showAgeFlag = true;
+      this.age = this.calculateAge();
+    },
+    calculateAge() {
+      const birthDate = new Date(this.user.dob);
+      const today = new Date();
+      const age = today.getFullYear() - birthDate.getFullYear();
+      return age;
     }
   }
 }).mount('.app')
